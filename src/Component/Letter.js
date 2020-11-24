a6d37ca0724bd1a255ed2455dcc377a7d7381647
Brandon Garcia <grandonleebarcia@gmail.com>
2020-11-23 17:48:13 -0800

    removed condtional css styling for now

--- src/Component/Letter.js
+++ src/Component/Letter.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import { useInputState } from '../Context'
 
-const letter = ({ children, bgColor }) => {
-    const charStyle = {
-        backgroundColor: bgColor
+const green = 'rgba(0, 255, 0, 0.7)';
+const red = 'rgba(255, 0, 0, 0.7)';
+const lightGray = 'lightgrey'
+
+
+
+const Letter = ({ children, char, i, j }) => {
+
+
+    const input = useInputState();
+
+
+
+    const checkChar = (char, i, j) => {
+        if (input[input.length - 1].split('')[j] == char && input.length - 1 == i) {
+
+
+        }
     }
 
+    let color = checkChar(char, i, j);
+
+    const charStyle = {
+        backgroundColor: color
+    }
+
+
     return (
-        <span style={charStyle}>
+        <span style={charStyle} >
 
             {children}
 
@@ -14,4 +37,4 @@ const letter = ({ children, bgColor }) => {
     )
 }
 
-export default letter
+export default Letter
