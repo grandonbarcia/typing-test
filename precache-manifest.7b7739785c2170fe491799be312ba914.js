49834b459561385f0a08e62b5b2393b62dd52f76
Brandon Garcia <grandonleebarcia@gmail.com>
2020-11-30 10:33:45 -0800

    Updates

---
+++ precache-manifest.7b7739785c2170fe491799be312ba914.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124e1e3eac877248e2651d0bde64b7c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7a273147a87c53b2abf",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "60ff2a8eacbf9815b9c3",
+    "url": "/static/css/main.3582f013.chunk.css"
+  },
+  {
+    "revision": "d7a273147a87c53b2abf",
+    "url": "/static/js/2.b3d35cc1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b3d35cc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ff2a8eacbf9815b9c3",
+    "url": "/static/js/main.33940f83.chunk.js"
+  },
+  {
+    "revision": "718ce3678678957d944d",
+    "url": "/static/js/runtime-main.03b02dac.js"
+  }
+]);
