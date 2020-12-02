f4b51ffba6bc834022f28a021c6cc8f1e7d3248d
Brandon Garcia <grandonleebarcia@gmail.com>
2020-12-02 12:39:18 -0800

    added admin login link

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -4,19 +4,17 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Navbar from 'react-bootstrap/Navbar'
 import Button from 'react-bootstrap/Button'
+import { Link } from "react-router-dom";
 
 export const Nav = () => {
     return (
         <Navbar expand="lg" variant="light" bg="light">
             <Container fluid={'lg'}>
                 <Col className="mr-auto">
-                    <Navbar.Brand href="#">Typing Test </Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/" href="#">Typing Test </Navbar.Brand>
                 </Col>
                 <Col xl={'auto'}>
-                    <Button variant="outline-primary" size={'sm'}>Login</Button>
-                </Col>
-                <Col xl={'auto'}>
-                    <Button variant="outline-secondary" size={'sm'} >Sign Up</Button>
+                    <Button as={Link} to="/login" variant="outline-danger" size={'sm'}>Admin</Button>
                 </Col>
             </Container>
         </Navbar>
