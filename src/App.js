460c3b3f321895125400d6d081e41b0bd015da1f
Brandon Garcia <grandonleebarcia@gmail.com>
2020-12-02 12:41:44 -0800

    added react router for admin login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container'
-import { Nav } from './Nav/Nav'
+import './App.css';
+import { Container, Row } from 'react-bootstrap'
+import { Nav as NavBar } from './Nav/Nav'
 import Quote from './Component/Quote'
 import Keyboard from './Component/Keyboard'
 import InputField from './Component/InputField'
 import Stats from './Component/Stats'
-import './App.css';
 import { InputProvider } from './Context/context';
+import TypingTest from './Component/TypingTest';
+import Login from './Pages/Login';
+import Admin from './Pages/Admin';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
       <InputProvider>
-        <Nav />
-        <Container className="h-75 " fluid={'xl'}>
-          <Quote />
-          <InputField />
-          <Stats />
-        </Container>
+        <Router>
+          <NavBar />
+          <Container className="h-100" fluid={'xl'}>
+            <Switch>
+              <Route exact path="/">
+                <TypingTest />
+              </Route>
+              <Route path="/login">
+                <Login />
+              </Route>
+              <Route path="/admin">
+                <Admin />
+              </Route>
+            </Switch>
+
+          </Container>
+        </Router>
       </InputProvider>
     </div >
   );
