b3e94feac746709449cb0b0f370773718f4239d9
Brandon Garcia <grandonleebarcia@gmail.com>
2020-12-10 14:26:27 -0800

    turns red when user enters incorrect letter

--- src/Component/Letter.js
+++ src/Component/Letter.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { useInputState } from '../Context'
 
-const green = 'rgba(0, 255, 0, 0.7)';
-const red = 'rgba(255, 0, 0, 0.7)';
+const green = 'rgba(0, 255, 0, 0.3)';
+const red = 'rgba(255, 0, 0, 0.3)';
 const lightGray = 'lightgrey'
 
 
@@ -13,18 +13,29 @@ const Letter = ({ children, char, i, j }) => {
     const input = useInputState();
 
 
+    const checkChar = () => {
+        let currInputChar = input[input.length - 1].split('')[j]
+        let currInputCharIndex = input[input.length - 1].split('').length - 1;
 
-    const checkChar = (char, i, j) => {
-        if (input[input.length - 1].split('')[j] == char && input.length - 1 == i) {
 
-
+        if (input.length - 1 == i) {
+            if (currInputChar === undefined) {
+                return green
+            } else if (currInputChar == char) {
+                return green
+            } else if (currInputChar !== char) {
+                return red
+            } else {
+                return red
+            }
         }
     }
 
-    let color = checkChar(char, i, j);
+    let color = checkChar();
 
     const charStyle = {
-        backgroundColor: color
+        backgroundColor: color,
+        fontSize: '25px'
     }
 
 
@@ -38,3 +49,9 @@ const Letter = ({ children, char, i, j }) => {
 }
 
 export default Letter
+
+
+// if (input[input.length - 1].split('')[j] == char && input.length - 1 == i) {
+
+//     return green
+// }
